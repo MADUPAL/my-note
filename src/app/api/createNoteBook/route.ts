eb3b7feae00b5234f0c2fdd6e17ba09af03a280a
madupal <kevin488@naver.com>
2025-01-19 23:46:10 +0900

    createNotebook route

---
+++ src/app/api/createNoteBook/route.ts
@@ -0,0 +1,15 @@
+// /api/createNoteBook
+
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("unauthorized", { status: 401 });
+  }
+  const body = await req.json();
+  const { name } = body;
+
+  return new NextResponse("ok");
+}
