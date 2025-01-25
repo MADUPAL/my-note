61a7777b7a29a0a5088cadd7b3a2df9626cac72e
madupal <kevin488@naver.com>
2025-01-25 23:33:36 +0900

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,22 @@
-"use client";
 import CreateNoteDialog from "@/components/CreateNoteDialog";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { db } from "@/server/db";
 import { UserButton, useUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-const Dashboard = (props: Props) => {
-  // const { user } = useUser();
-
-  // <div>{user?.firstName}</div>
+const Dashboard = async (props: Props) => {
+  const { userId } = await auth();
+  const notes = await db.note.findMany({
+    where: {
+      userId: userId!,
+    },
+  });
   return (
     <>
       <div className="grainy min-h-screen">
@@ -37,12 +41,40 @@ const Dashboard = (props: Props) => {
           <div className="h-8"></div>
           {/* list all the notes */}
           {/* TODO conditionally rendered */}
-          <div className="text-center">
-            <h2 className="text-xl text-gray-500">You have no Notes yet</h2>
-          </div>
+          {notes.length === 0 && (
+            <div className="text-center">
+              <h2 className="text-xl text-gray-500">You have no Notes yet</h2>
+            </div>
+          )}
+
           {/* display all notes */}
           <div className="grid grid-cols-1 gap-3 sm:grid-cols-3 md:grid-cols-5">
             <CreateNoteDialog />
+
+            {notes.map((note) => {
+              return (
+                <a href={`/notes/${note.id}`} key={note.id}>
+                  <div className="flex flex-col overflow-hidden rounded-lg border border-stone-200 transition hover:-translate-y-1 hover:shadow-xl">
+                    <img
+                      width={400}
+                      height={200}
+                      alt={note.name}
+                      src="public/1772-pepe-present.png"
+                    />
+                    <div className="p-4">
+                      <h3 className="text-xl font-semibold text-gray-900">
+                        {note.name}
+                      </h3>
+                      <div className="h-1">
+                        <p className="text-sm text-gray-500">
+                          {new Date(note.createdAt).toLocaleDateString()}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
           </div>
         </div>
       </div>
